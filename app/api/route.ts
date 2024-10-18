0b3315101e9be5c07113b95e70b3f5d123da277f
Nishant | dev <69218488+NishantCoder108@users.noreply.github.com>
2024-10-18 09:30:02 +0000

    Get Post for blink setup

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,15 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
     try {
-        // const searchParams = req.nextUrl.searchParams;
-        // const userId = searchParams.get("userId");
-
-              return NextResponse.json(
-            {
-               message :"Hello Nishant!"
-            },
-            { status: 200 }
-        );
+       
     } catch (error) {
         console.log({ error });
 
